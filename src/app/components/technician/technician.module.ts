6f02eed21690e63b5fad8622d07af7273d4278ca
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-06-07 06:37:26 -0400

    Se agrega componente de listad de tecnicos

--- src/app/components/technician/technician.module.ts
+++ src/app/components/technician/technician.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ListTechnicianComponent } from './list-technician/list-technician.component'
+import { FormTechnicianComponent } from './form-technician/form-technician.component';
+import { ModalDeleteTechnicianComponent } from './modal-delete-technician/modal-delete-technician.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListTechnicianComponent, FormTechnicianComponent, ModalDeleteTechnicianComponent],
+  exports: [
+    ListTechnicianComponent,
+    FormTechnicianComponent,
+    ModalDeleteTechnicianComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class TechnicianModule { }
--- src/app/pages-admin/admin-technician/admin-technician.module.ts
+++ src/app/pages-admin/admin-technician/admin-technician.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { AdminTechnicianPageRoutingModule } from './admin-technician-routing.module';
 
 import { AdminTechnicianPage } from './admin-technician.page';
+import { TechnicianModule } from 'src/app/components/technician/technician.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AdminTechnicianPageRoutingModule
+    AdminTechnicianPageRoutingModule,
+    TechnicianModule
+
   ],
   declarations: [AdminTechnicianPage]
 })
-export class AdminTechnicianPageModule {}
+export class AdminTechnicianPageModule { }
