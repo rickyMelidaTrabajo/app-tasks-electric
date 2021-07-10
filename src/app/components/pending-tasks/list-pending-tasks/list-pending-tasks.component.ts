ee55f25a2c7731bf069d0b3adb09515da1037560
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-07-09 22:49:54 -0400

    Se agrega scroll infinite
     Se agrefa infinite scroll en lista de tareas pendientes

--- src/app/components/pending-tasks/list-pending-tasks/list-pending-tasks.component.ts
+++ src/app/components/pending-tasks/list-pending-tasks/list-pending-tasks.component.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/member-ordering */
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { LoadingController, ModalController } from '@ionic/angular';
 import { Task } from 'src/app/models/task.interface';
 import { FormClosePendingComponent } from '../form-close-pending/form-close-pending.component';
 import { DetailPendingTaskComponent } from '../detail-pending-task/detail-pending-task.component';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-list-pending-tasks',
@@ -12,10 +13,12 @@ import { DetailPendingTaskComponent } from '../detail-pending-task/detail-pendin
   styleUrls: ['./list-pending-tasks.component.scss'],
 })
 export class ListPendingTasksComponent implements OnInit {
-  tasks: Array<any> = new Array();
+  @ViewChild(IonInfiniteScroll, { static: false }) infiniteScroll: IonInfiniteScroll;
   @Input() data: Array<Task>;
-  //@Input() data: Array<any>;
+  tasks: Array<any> = new Array();
 
+  tasksPartial: Array<Task> = new Array();
+  index = 0;
   constructor(private modalController: ModalController, private loadingController: LoadingController) { }
 
   ngOnInit() {
@@ -51,8 +54,6 @@ export class ListPendingTasksComponent implements OnInit {
     return await modal.present();
   }
 
-
-
   async loading() {
     const loading = await this.loadingController.create({
       cssClass: 'my-custom-class',
@@ -62,6 +63,28 @@ export class ListPendingTasksComponent implements OnInit {
     await loading.present();
 
     const { role, data } = await loading.onDidDismiss();
-    this.data;
+    this.loadPartialData(this.index);
+    console.log(this.data);
+  }
+
+  loadData(event) {
+    if (this.tasksPartial.length === this.data.length) {
+      event.target.complete();
+      this.infiniteScroll.disabled = true;
+      return;
+    }
+
+    setTimeout(() => {
+      this.loadPartialData(this.index);
+      event.target.complete();
+    }, 1000);
+
+    this.index = + 5;
+  }
+
+  loadPartialData(lastItem) {
+    for (let i = lastItem; i < lastItem + 5; i++) {
+      this.tasksPartial.push(this.data[i]);
+    }
   }
 }
